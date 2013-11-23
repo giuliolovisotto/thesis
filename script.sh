fdb8fda76aa5e59f8cdfe8dc6513c647e75b1c77
Giulio <giuliolovisotto@gmail.com>
2013-11-23 20:22:02 +0100

    everthing

--- script.sh
+++ script.sh
@@ -6,6 +6,7 @@ wget -a log.out http://localhost/comp_class.php ;
 wget -a log.out http://localhost/usecase.php ;
 wget -a log.out http://localhost/tr_req_uc.php ;
 wget -a log.out http://localhost/req.php ;
+wget -a log.out http://localhost/esitistatica.php ;
 echo "Requests sent, check log.out for output";
 rm ./*.php* ;
 echo "Sleep for 3 sec hoping that everything gets updated..";
@@ -20,6 +21,7 @@ mv /var/www/specifica.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections
 mv /var/www/requisiti.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections/requisiti.tex
 mv /var/www/casiduso.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections/casiduso.tex
 mv /var/www/trReqUc.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections/trReqUc.tex
+mv /var/www/esitistatica.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections/esitistatica.tex
 
 echo "Now generating pdflatex..";
 pdflatex -halt-on-error -synctex=1 -output-format='pdf' -output-directory='./build' -interaction=nonstopmode ../thesis.tex >> log.out
