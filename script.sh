27b545ad009b949ec1f8687654b89a6e1c8b0f70
Giulio <giuliolovisotto@gmail.com>
2013-11-26 01:25:56 +0100

    V_5!

--- script.sh
+++ script.sh
@@ -7,6 +7,12 @@ wget -a log.out http://localhost/usecase.php ;
 wget -a log.out http://localhost/tr_req_uc.php ;
 wget -a log.out http://localhost/req.php ;
 wget -a log.out http://localhost/esitistatica.php ;
+wget -a log.out http://localhost/esitoreq.php ;
+wget -a log.out http://localhost/esitovalidazione.php ;
+wget -a log.out http://localhost/tests.php ;
+
+
+
 echo "Requests sent, check log.out for output";
 rm ./*.php* ;
 echo "Sleep for 3 sec hoping that everything gets updated..";
@@ -22,6 +28,9 @@ mv /var/www/requisiti.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections
 mv /var/www/casiduso.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections/casiduso.tex
 mv /var/www/trReqUc.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections/trReqUc.tex
 mv /var/www/esitistatica.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections/esitistatica.tex
+mv /var/www/testvalidaz.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections/testvalidaz.tex
+mv /var/www/esitoreq.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections/esitoreq.tex
+mv /var/www/esitovalidazione.tex /home/giulio/documents/UNIVERSITA/tesi/thesis/sections/esitovalidazione.tex
 
 echo "Now generating pdflatex..";
 pdflatex -halt-on-error -synctex=1 -output-format='pdf' -output-directory='./build' -interaction=nonstopmode ../thesis.tex >> log.out
